4e76e704549a00ca1a79680c7dbed779b07d70f3
lockwoodp175 <paullockwood99@gmail.com>
2025-08-04 18:12:49 +0100

    wireup file parser and simulator

--- RobotSimulator/Program.cs
+++ RobotSimulator/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace RobotSimulator
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                InputFileParser p = new InputFileParser();
+                await p.Parse(args[0]);
+
+                Simulator rr = new Simulator(p.Width, p.Height);
+                foreach (var i in p.Instructions)
+                {
+                    RobotState state = rr.Execute(i.StartX, i.StartY, i.Orientation, i.Instructions);
+
+                    string lost = state.Lost ? "LOST" : "";
+                    Console.WriteLine($"{state.X} {state.Y} {state.Orientation} {lost}");
+                }
+
+                Console.WriteLine("Press enter to exit simulation");
+                Console.ReadLine();
+            } catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading input file: {ex.Message}");
+            }
         }
     }
 }
