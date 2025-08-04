fe0008e197dd7e3334d4225cd2d1ae0cfad66c49
lockwoodp175 <paullockwood99@gmail.com>
2025-08-04 18:41:45 +0100

    add sample file parser tests

---
+++ RobotSimulatorTests/InputFileParserTests.cs
@@ -0,0 +1,40 @@
+﻿using RobotSimulator;
+using RobotSimulator.Instructions;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RobotSimulatorTests
+{
+    public class InputFileParserTests
+    {
+        [Fact]
+        public void MissingInputFileThrowsIOException()
+        {
+            // Arrange
+            var sut = new InputFileParser();
+
+            // Act
+            _ = Assert.ThrowsAsync<IOException>(async () => { await sut.Parse("missingFile"); });
+        }
+
+        [Fact]
+        public async Task Parse_TooManyArgs_ThrowsException()
+        {
+            // Arrange
+            var sut = new InputFileParser();
+            using MemoryStream s = new MemoryStream();
+            using StreamWriter w = new StreamWriter(s);
+            w.WriteLine("x y n");
+            w.Flush();
+            s.Position = 0;
+            StreamReader r = new StreamReader(s);
+
+            // Act
+            Exception ex = await Assert.ThrowsAsync<Exception>(async () => { await sut.Parse(r); });
+
+            Assert.Equal("Incorrect grid size parameters x y n", ex.Message);
+        }
+    }
+}
